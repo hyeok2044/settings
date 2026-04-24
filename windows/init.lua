811caebf41174f3a8cee3052b9a66ca3bdc81faa
hyeok2044 <hyeok2044@gmail.com>
2026-04-24 20:58:42 +0900

    added file path copy rules

--- windows/init.lua
+++ windows/init.lua
@@ -1,3 +1,18 @@
+-- Fix broken Lua module search path (Windows env/Lua interference)
+-- Append all runtimepath/lua folders to package.path so require() can find plugin modules.
+-- do
+--   local sep = package.config:sub(1, 1)
+--   local function add_lua_paths(p)
+--     local lua_dir = p .. sep .. "lua"
+--     package.path = package.path
+--       .. ";" .. lua_dir .. sep .. "?.lua"
+--       .. ";" .. lua_dir .. sep .. "?" .. sep .. "init.lua"
+--   end
+-- 
+--   for _, p in ipairs(vim.api.nvim_list_runtime_paths()) do
+--     add_lua_paths(p)
+--   end
+-- end
 -- 1. Bootstrap lazy.nvim
 local lazypath = vim.fn.stdpath("data") .. "/lazy/lazy.nvim"
 if not (vim.uv or vim.loop).fs_stat(lazypath) then
@@ -101,7 +116,83 @@ require("lazy").setup({
         })
       end,
     },
+
+    {
+      "nvim-treesitter/nvim-treesitter",
+      lazy = false,
+      build = ":TSUpdate",
+      config = function()
+        require("nvim-treesitter.config").setup({
+          ensure_installed = { "php" },
+          highlight = { enable = true },
+          indent = { enable = true },
+        })
+      end,
+    },
+    {
+      "akinsho/toggleterm.nvim",
+      version = "*",
+      config = function()
+        require("toggleterm").setup({
+          -- 1. 사용할 단축키 설정
+          open_mapping = [[<C-\>]], 
+          -- 2. 터미널이 뜨는 방식 ('float'가 깔끔합니다)
+          direction = 'float', 
+          -- 3. 터미널이 열릴 때 자동으로 입력 모드로 전환
+          start_in_insert = true,
+          -- 4. 플로팅 창 스타일 (선택 사항)
+          float_opts = {
+            border = "curved", -- 테두리 모양
+          },
+        })
+
+        -- 터미널 모드에서 탈출하거나 이동하기 편한 키맵 설정
+        function _G.set_terminal_keymaps()
+          local opts = {buffer = 0}
+          -- Esc 대신 jk로 터미널 노멀 모드 전환 (원하는 대로 수정 가능)
+          vim.keymap.set('t', '<esc>', [[<C-\><C-n>]], opts)
+          vim.keymap.set('t', 'jk', [[<C-\><C-n>]], opts)
+          -- 터미널 안에서 창 이동
+          vim.keymap.set('t', '<C-h>', [[<Cmd>wincmd h<CR>]], opts)
+          vim.keymap.set('t', '<C-j>', [[<Cmd>wincmd j<CR>]], opts)
+          vim.keymap.set('t', '<C-k>', [[<Cmd>wincmd k<CR>]], opts)
+          vim.keymap.set('t', '<C-l>', [[<Cmd>wincmd l<CR>]], opts)
+        end
+
+        -- 터미널이 열릴 때만 위 키맵 적용
+        vim.cmd('autocmd! TermOpen term://* lua set_terminal_keymaps()')
+      end,
+    },
+    { 'RaafatTurki/hex.nvim', config = true },
+    {
+        "tpope/vim-dadbod",
+        dependencies = {
+          "kristijanhusak/vim-dadbod-ui",
+          "kristijanhusak/vim-dadbod-completion",
+        },
+        config = function()
+          -- vim-dadbod-ui 관련 설정
+          vim.g.db_ui_save_location = "~/.config/nvim/db_ui" -- UI 설정 저장 위치
+          vim.g.db_ui_show_help = 0                         -- 도움말 숨기기 (깔끔하게)
+          vim.g.db_ui_win_width = 30                        -- 왼쪽 사이드바 너비
+
+          -- 자동완성 연동 (sql 파일에서 작성 시 자동완성 활성화)
+          vim.api.nvim_create_autocmd("FileType", {
+            pattern = { "sql", "mysql", "plsql" },
+            callback = function()
+              require("cmp").setup.buffer({ sources = { { name = "vim-dadbod-completion" } } })
+            end,
+          })
+        end,
+        keys = {
+          { "<leader>du", "<cmd>DBUIToggle<cr>", desc = "DB UI 토글" },
+          { "<leader>df", "<cmd>DBUIFindBuffer<cr>", desc = "현재 버퍼 DB 찾기" },
+          { "<leader>dr", "<cmd>DBUIRenameBuffer<cr>", desc = "DB 버퍼 이름 변경" },
+          { "<leader>dl", "<cmd>DBUILastQueryInfo<cr>", desc = "마지막 쿼리 정보" },
+        },
+      },
   },
+  -- autofold
 })
 
 -- 3. 유니티 자동 CD 설정
@@ -126,3 +217,31 @@ vim.opt.tabstop = 2
 vim.opt.shiftwidth = 2
 vim.opt.expandtab = true
 vim.opt.clipboard:append("unnamedplus")
+
+
+-- Folding
+vim.opt.foldmethod = "expr"
+vim.opt.foldexpr = "v:lua.vim.treesitter.foldexpr()"
+vim.opt.foldlevel = 99
+vim.opt.foldenable = true
+
+-- Full path → 클립보드
+vim.keymap.set('n', '<leader>fp', function()
+  local path = vim.fn.expand('%:p')
+  vim.fn.setreg('+', path)
+  print('Copied full path: ' .. path)
+end, { desc = 'Copy full path' })
+
+-- Relative path → 클립보드
+vim.keymap.set('n', '<leader>rp', function()
+  local path = vim.fn.expand('%')
+  vim.fn.setreg('+', path)
+  print('Copied relative path: ' .. path)
+end, { desc = 'Copy relative path' })
+
+-- File name only → 클립보드
+vim.keymap.set('n', '<leader>fn', function()
+  local name = vim.fn.expand('%:t')
+  vim.fn.setreg('+', name)
+  print('Copied file name: ' .. name)
+end, { desc = 'Copy file name' })
